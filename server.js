06f78f31f79be6d6a912662e704b7e2886c0b425
misschootemup <misschootemup@gmail.com>
2023-12-22 10:01:59 -0600

    added routes

--- server.js
+++ server.js
@@ -12,4 +12,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 
+//hompage route
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+
+
+
+
 app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
