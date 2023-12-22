68ef0b32c74d484b03edcf7478a8adee2280d8b2
misschootemup <misschootemup@gmail.com>
2023-12-22 09:53:31 -0600

    created server

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 const express = require('express');
 const path = require('path');
+const { readFile, writeFile } = require('fs').promises;
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+
+app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
