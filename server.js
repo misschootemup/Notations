0c26e5dee27669eac92260de92907b33973f0c6c
misschootemup <misschootemup@gmail.com>
2024-01-02 18:59:53 -0600

    finished

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { readFile, writeFile } = require('fs').promises;
+var uniqid = require('uniqid'); 
 
 const PORT = process.env.PORT || 3001;
 
@@ -20,6 +21,24 @@ app.get("/api/notes", (req, res) =>{
         res .json(notes)
     })
 })
+
+app.post ("/api/notes", (req, res)=>{
+    console.log (req.body)
+    readFile("./db/db.json","utf8")
+    .then((data)=>{
+        let notes = JSON.parse(data)
+        console.log (notes)
+        var newNote={
+            ...req.body,
+            id : uniqid ()
+        }
+        notes.push(newNote)
+        return writeFile('./db/reviews.json', JSON.stringify(parsedReviews, null, 4));
+
+       // res .json(notes)
+    })
+})
+
 
 //hompage route
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
